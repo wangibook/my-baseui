fe15b0e605ea24948a017c4f64fedf3083f979b1
dashi <297901039@qq.com>
2022-06-14 10:52:56 +0800

    构建命令修改

---
+++ build/base.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import Markdown from 'vite-plugin-md';
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown(),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      '~': resolve(__dirname, "packages")
+    }
+  },
+})
---
+++ build/dist.config.js
@@ -0,0 +1,22 @@
+import baseConfig from './base.config';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  ...baseConfig,
+  build: {
+    rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖
+      external: ["vue"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: "Vue",
+        }
+      },
+    },
+    lib: {
+      entry: "./packages/index.js",
+      name: "my-baseui",
+    },
+  }
+});
