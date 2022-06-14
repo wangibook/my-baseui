7267ea060f0ee409fea9500bd82cc112d05bb4f0
dashi <297901039@qq.com>
2022-06-14 09:00:06 +0800

    tj

--- build/build.js
+++ build/build.js
@@ -3,6 +3,7 @@ import Vue from "@vitejs/plugin-vue";
 import Markdown from "vite-plugin-md";
 import { resolve } from "path";
 export default defineConfig({
+  base: '/my-baseui/',
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/],
