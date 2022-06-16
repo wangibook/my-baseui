32c649c2d20a9ba7bdba3065328f1b14d905c48a
dashi <297901039@qq.com>
2022-06-16 11:57:07 +0800

    包过大时

--- build/docs.config.js
+++ build/docs.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   base: '/my-baseui/',
   build: {
     outDir: 'docs',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      },
+    },
   },
 });
