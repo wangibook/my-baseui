e7786a2a546f3c5fba842396fab5f9c35ddf4657
dashi <297901039@qq.com>
2022-07-14 14:47:45 +0800

    deploy

---
+++ components/card/doc/demo1.vue
@@ -0,0 +1,28 @@
+<template>
+  <m-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>卡片名称</span>
+        <m-button class="button" type="text">操作按钮</m-button>
+      </div>
+    </template>
+    <div v-for="item in 4" :key="item" class="item">
+      {{ '列表内容 ' + item }}
+    </div>
+  </m-card>
+</template>
+
+<style>
+.box-card {
+  width: 480px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.item {
+  font-size: 14px;
+  margin-bottom: 18px;
+}
+</style>
---
+++ components/card/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-card" :class="shadowClass">
+    <div class="m-card__header" v-if="$slots.header || header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div class="m-card__body" :style="bodyStyle">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mCard"
+};
+</script>
+
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+  header: {
+    type: String,
+    default: '',
+  },
+  bodyStyle: Object,
+  shadow: {
+    type: String,
+    default: 'always'
+  }
+})
+
+const shadowClass = computed(() => {
+  return {
+    [`is-shadow-${props.shadow}`]: props.shadow
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/components/card.scss';
+</style>
