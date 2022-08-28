595ee6027455d3f744be2911aeabcb3a454283f1
Gridfas <809418027@qq.com>
2022-08-28 22:51:40 +0800

    更新全部组件内容导航json文件

--- src/router/routerConfig/navigation.component.js
+++ src/router/routerConfig/navigation.component.js
@@ -1,17 +1,24 @@
+import breadcrumbContent from '/packages/components/breadcrumb/doc/content.json';
+import bdropdownContent from '/packages/components/dropdown/doc/content.json';
+import tabsContent from '/packages/components/tabs/doc/content.json';
+
 export const navigationComponent = [
   {
     path: '/breadcrumb',
     name: 'Breadcrumb 面包屑',
+    content: breadcrumbContent,
     component: () => import('/packages/components/breadcrumb/doc/doc.md')
   },
   {
     path: '/dropdown',
     name: 'Dropdown 下拉菜单',
+    content: bdropdownContent,
     component: () => import('/packages/components/dropdown/doc/doc.md')
   },
   {
     path: '/tabs',
     name: 'Tabs 标签页',
+    content: tabsContent,
     component: () => import('/packages/components/tabs/doc/doc.md')
   },
 ]
